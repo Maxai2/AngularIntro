d3fe3896be201a5e5aa5e8ce2fc753cec8e66306
Махмудов Али Ильгар <Mahm_iz81@itstep.edu.az>
2019-02-04 11:11:18 +0400

    auth-interceptor

--- ClientApi/src/app/services/auth-interceptor.ts
+++ ClientApi/src/app/services/auth-interceptor.ts
@@ -1,7 +1,7 @@
 import { HttpInterceptor, HttpEvent, HttpHandler, HttpRequest, HttpHeaders, HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
 import { Injectable } from '@angular/core';
-import { catchError } from 'rxjs/operators';
+import { catchError, flatMap } from 'rxjs/operators';
 import { LoginResponse } from '../models/login-response';
 
 @Injectable()
@@ -12,28 +12,46 @@ export class AuthInterceptor implements HttpInterceptor {
     constructor(private http: HttpClient) {}
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        const accessToken = localStorage.getItem('accessToken');
+        let accessToken = localStorage.getItem('accessToken');
         if (accessToken) {
             const h = new HttpHeaders().set('Authorization', 'Bearer ' + accessToken);
             req = req.clone({ headers: h });
         }
-        return next.handle(req).pipe(catchError(
-            (err) => {
-                if (err.status === 401) {
-                    const refreshToken = localStorage.getItem('refreshToken');
-                    if (refreshToken) {
-                        this.http.post<LoginResponse>(`${this.url}account/token`, { refreshToken: refreshToken} ).subscribe(
-                            (result) => {
-                                console.log(result);
-                            },
-                            (error) => {
-                                console.log(error);
-                            }
-                        );
-                    }
+        // return next.handle(req).pipe(catchError(
+        //     (err) => {
+        //         if (err.status === 401) {
+        //             const refreshToken = localStorage.getItem('refreshToken');
+        //             if (refreshToken) {
+        //                 this.http.post<LoginResponse>(`${this.url}account/token`, { refreshToken: refreshToken} ).subscribe(
+        //                     (result) => {
+        //                         console.log(result);
+        //                     },
+        //                     (error) => {
+        //                         console.log(error);
+        //                     }
+        //                 );
+        //             }
+        //         }
+        //         throw err;
+        //     }
+        // ));
+        return next.handle(req).pipe(catchError((err) => {
+            if (err.status === 401) {
+                const refreshToken = localStorage.getItem('refreshToken');
+                if (refreshToken) {
+                    return this.http.post<LoginResponse>(`${this.url}account/token`, { refreshToken: refreshToken })
+                        .pipe(flatMap((result) => {
+                            localStorage.clear();
+                            localStorage.setItem('accessToken', result.accessToken);
+                            localStorage.setItem('refreshToken', result.refreshToken);
+                            accessToken = result.accessToken;
+                            const h = new HttpHeaders().set('Authorization', 'Bearer ' + accessToken);
+                            req = req.clone({ headers: h });
+                            return next.handle(req);
+                        }));
                 }
                 throw err;
             }
-        ));
+        }));
     }
 }
